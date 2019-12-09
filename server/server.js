9a24d401c0c183522efa1c1008b5459cb22794b1
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-12-09 14:52:18 +0100

    json write

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 6000
+const fs = require('fs');
+// function for writing all locations to location.json file 
+function updatejson(locationFetch) {
+    let locations = JSON.stringify(locationFetch);
+    fs.writeFileSync('location.json', locations);
+}
 
 app.use(express.static('public'))
 
