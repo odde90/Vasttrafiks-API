727db52e42c30a8dd9b16fa44dd7345f37a0d138
Lars Nihlmark <larsnihlmark@gmail.com>
2019-12-09 15:08:09 +0100

    feature/express server up in localhost port 5000

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 6000
+var express = require('express');
+var app = express();
+var path = require('path');
 
-app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+  app.listen(5000, function () {
+    console.log('Node server is running..');
+});
