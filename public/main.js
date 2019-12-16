47f9ed640e6de47f771bbaa117db2277067b358b
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-12-16 16:13:00 +0100

    Co-authored-by: odde <ah1995hu@gmail.com>

--- public/main.js
+++ public/main.js
@@ -1,7 +1,30 @@
-
-
 var suggestions = []
-var stops = ["Vasaplatsen", "Valand", "Brunsparken", "Censtralstationen","Saxofonvägen"];
+var stops = [];
+
+async function initLocation() {
+    try {
+        let response = await makeRequest();
+    } catch (er) {
+        console.error(er);
+    }
+}
+
+initLocation();
+
+async function makeRequest() {     
+    url = '/init';
+    let res = await fetch(url)
+    if (res.status != 200) {
+        throw new Error(response.status + ' ' + response.statusText)
+    } else {
+        let dataStop = await res.json();
+       /// console.log(dataStop)
+        dataStop.forEach(stop => {
+            stops.push(stop);
+        });
+    }
+}
+
 
 $( document ).ready(function() {
   //Hantering för inputFrån
@@ -9,33 +32,60 @@ $( document ).ready(function() {
         var input = document.getElementById('inputFrån');
         var filter = input.value.toUpperCase();
         var ul = document.getElementById("FromUl");
-        if( $("#inputFrån").val() != ''){
-            ul.style.display = 'block'
-        }else{
+
+        if( $("#inputFrån").val() != '' ){
+                $(document).on('keypress',function(e) {
+                    if(e.which == 13) {
+                        ul.style.display = 'none'
+                    }else{
+                        $("#inputFrån").on("keydown", function() {
+
+                            ul.style.display = 'block'
+                        });
+                    }
+                });
+    
+                $(document).on("click","#FromUl",function() {
+                    ul.style.display = 'none'
+                });
+        } 
+         else{
             ul.style.display = 'none'
-        }
+        } 
         var bigletter = [];
-        stops.forEach(ext => {
-            bigletter.push(ext.toUpperCase())
-        });
-      printFrom(bigletter,filter);
-     
+        const filteredStops = {};
+      
+        stops.forEach(stop => {
+            filteredStops[stop._attributes.name.toUpperCase()] = stop._attributes.weight
+        })
+        
+        for (const name in filteredStops) {
+            var stop = {
+              name,
+              weigth: filteredStops[name]
+            }
+            bigletter.push(stop)
+        }
+        printFrom(bigletter, filter);
+
     });
     function printFrom(sugs,filter) {
         var finsihsingalex = [];
         ul1 = document.getElementById("FromUl");
         var input = document.getElementById('inputFrån');
         ul1.innerText = '';
+        var counter = 0;
         sugs.forEach(e => {
-            if (e.indexOf(filter) > -1) {
-                finsihsingalex.push(e);
+            if (e.name.indexOf(filter) > -1 && counter < 6 ) {
+                counter++
+            finsihsingalex.push(e.name);
             var  liItem = document.createElement('li');
             var atag = document.createElement('a'); 
             liItem.addEventListener("click", function() {
               inputFrom = document.getElementById('inputFrån');
-              inputFrom.value = e;
+              inputFrom.value = e.name;
             });
-            atag.innerText = e;
+            atag.innerText = e.name;
             liItem.append(atag);
             ul1.append(liItem) 
           } 
@@ -46,8 +96,10 @@ $( document ).ready(function() {
                 ul1 = document.getElementById("FromUl");
                 var inputFrom = document.getElementById('inputFrån');
                 inputFrom.value =  finsihsingalex[0]
-                ul1.style.display = 'none'
+                //ul1.style.display = 'none'
                 ul1.innerText = '';
+                ul1.style.display = null;
+                
             }
         });
     }
@@ -57,16 +109,45 @@ $( document ).ready(function() {
             var input = document.getElementById('inputTill');
             var filter = input.value.toUpperCase();
             var ul = document.getElementById("toUl");
-            if( $("#inputTill").val() != ''){
-                ul.style.display = 'block'
-            }else{
-                ul.style.display = 'none'
+
+            if( $("#inputTill").val() != '' ){
+                $(document).on('keypress',function(e) {
+                    if(e.which == 13) {
+                        ul.style.display = 'none'
+                    }else{
+                        $("#inputTill").on("keydown", function() {
+
+                            ul.style.display = 'block'
+                        });
+                    }
+                });
+    
+                $(document).on("click","#FromUl",function() {
+                    ul.style.display = 'none'
+                });
             }
-            var bigletter = [];
-            stops.forEach(ext => {
-                bigletter.push(ext.toUpperCase())
-            });
-          printTo(bigletter,filter);
+
+            else{
+                ul.style.display = 'none'
+            } 
+
+            var tobigletter = [];
+            const tofilteredStops = {};
+          
+            stops.forEach(stop => {
+                tofilteredStops[stop._attributes.name.toUpperCase()] = stop._attributes.weight
+            })
+            
+            for (const name in tofilteredStops) {
+                var stop = {
+                  name,
+                  weigth: tofilteredStops[name]
+                }
+                tobigletter.push(stop)
+            }
+
+      
+          printTo(tobigletter,filter);
          
         });
         function printTo(sugs,filter) {
@@ -75,27 +156,31 @@ $( document ).ready(function() {
             ul1 = document.getElementById("toUl");
             var input = document.getElementById('inputTill');
             ul1.innerText = '';
+            var tocounter =  0;
             sugs.forEach(e => {
-                if (e.indexOf(filter) > -1) {
-                    finsihsingalex.push(e);
+
+                if (e.name.indexOf(filter) > -1 && tocounter < 6) {
+                    tocounter++
+                    finsihsingalex.push(e.name);
                 var  liItem = document.createElement('li');
                 var atag = document.createElement('a'); 
                 liItem.addEventListener("click", function() {
                   inputFrom = document.getElementById('inputTill');
-                  inputFrom.value = e;
+                  inputFrom.value = e.name;
                 });
-                atag.innerText = e;
+                atag.innerText = e.name;
                 liItem.append(atag);
                 ul1.append(liItem) 
               } 
             });
             input.addEventListener("keydown", function (e) {
                 
-                if (e.keyCode === 13) { 
-                    console.log(finsihsingalex[0])
+                if (e.keyCode === 13) {  
+                  
                     var inputFrom = document.getElementById('inputTill');
                     inputFrom.value =  finsihsingalex[0]
-                   
+                    
+
                 }
             });
         }
